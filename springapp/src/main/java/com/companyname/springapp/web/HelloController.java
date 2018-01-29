569e344241a5f7bc20d703f378cdd66a3984d12e
Curso mañana <Curso maana@PC04a5>
2018-01-29 14:40:06 +0100

    Spring

---
+++ springapp/src/main/java/com/companyname/springapp/web/HelloController.java
@@ -0,0 +1,40 @@
+package com.companyname.springapp.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+	
+	protected final Log logger = LogFactory.getLog(getClass());
+
+    @RequestMapping(value="/hello.htm")/*Esto hace que retorna al hello.jsp*/
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        logger.info("Returning hello view");
+
+        return new ModelAndView("hello.jsp");
+
+}
+    
+    @RequestMapping(value="/servicios.htm")/*Esto hace que retorna al hello.jsp*/
+    public ModelAndView llamarServicios(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        logger.info("Nuestros Servicios");
+
+        return new ModelAndView("servicios.jsp");
+
+}
+
+}
---
+++ springapp/src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<html>
+<head><title>Example :: Spring Application</title></head>
+<body>
+<h2>Spring-Example</h2>
+<p>This is my example</p>
+<a href="servicios.jsp">Ir a servicios</a>
+<a href="servicios.htm">Ir a .........</a>
+</body>
+</html>
